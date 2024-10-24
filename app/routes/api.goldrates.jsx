cec999c00791b884bf015c96b0a4d2dc255c382a
Sooraj Sharma <93427855+mymasterpices@users.noreply.github.com>
2024-10-24 15:20:31 +0530

    Update api.goldrates.jsx

--- app/routes/api.goldrates.jsx
+++ app/routes/api.goldrates.jsx
@@ -1,24 +1,12 @@
 import db from "../db.server";
-import { cors } from 'remix-utils/cors';
 
 export async function loader() {
-    try {
-        const current_rate = await db.goldGSTRates.findFirst();
+    const current_rate = await db.goldGSTRates.findFirst();
 
-        if (!current_rate) {
-            return new Response("Something went wrong...", { status: 500 });
-        }
-
-        // Assuming you want to return JSON data; adjust according to your needs
-        return new Response(JSON.stringify(current_rate), {
-            status: 200,
-            headers: {
-                "Content-Type": "application/json",
-                ...cors(), // Include CORS headers if needed
-            },
-        });
-    } catch (error) {
-        console.error("Error fetching gold rates:", error);
-        return new Response("Internal Server Error", { status: 500 });
+    if (!current_rate) {
+        return "Something went wrong...";
+    }
+    else {
+        return current_rate;
     }
 }
