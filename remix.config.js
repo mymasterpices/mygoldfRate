2e0372acb3c6dc22e0df981fd641f52ad2ea6a56
Sooraj Sharma <93427855+mymasterpices@users.noreply.github.com>
2024-10-24 14:46:32 +0530

    Update remix.config.js

--- remix.config.js
+++ remix.config.js
@@ -17,6 +17,9 @@ module.exports = {
   serverModuleFormat: "cjs",
   dev: { port: process.env.HMR_SERVER_PORT || 8002 },
   future: {},
+  serverDependenciesToBundle: [
+		/^remix-utils.*/,
+  ]
 };
 
 // remix.config.js
